b08364fc51d012f7ab75bad15b9756e142ffc168
krismina la <krismina.la@gmail.com>
2022-03-17 21:42:03 +0000

    removed unnecessary code

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,7 +4,7 @@ const fetchBreedDescription = function(breedName, callback) {
   const breedURL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
   
   request(breedURL, (error, response, body) => {
-    if (error) return console.log('Request Failed: ', error);
+    if (error) return error;
     
     const data = JSON.parse(body);
     
