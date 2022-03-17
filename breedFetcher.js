3a86d7bd5aada5ae049ad55eba42c93b307d5f44
krismina la <krismina.la@gmail.com>
2022-03-17 20:46:12 +0000

    program that fetches breed specified by user from API and returns description of breed

---
+++ breedFetcher.js
@@ -0,0 +1,13 @@
+const request = require('request');
+
+const breed = process.argv.slice(2).join('').substring(0,4);
+const breedURL = 'https://api.thecatapi.com/v1/images/search?breed_ids=' + breed;
+
+
+request(breedURL, (error, response, body) => {
+  if (error) return console.log('Request Failed: ', error);
+  
+  const data = JSON.parse(body);
+  if (data.length === 0) return console.log('Error: Breed not found.');
+  console.log(data[0].breeds[0].description);
+});
