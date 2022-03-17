0656c3a0dc9fb0ea2c100887b1d3e76a23209f42
krismina la <krismina.la@gmail.com>
2022-03-17 21:27:43 +0000

    refactored code and created a callback function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,22 @@
 const request = require('request');
 
-// breed ID is first 4 letters of breed name
-const breed = process.argv.slice(2).join('').substring(0,4);
-const breedURL = 'https://api.thecatapi.com/v1/images/search?breed_ids=' + breed;
+// breed ID is first 4 letters of breed
+//const breedName = process.argv.slice(2).join('').substring(0,4);
 
-
-request(breedURL, (error, response, body) => {
-  if (error) return console.log('Request Failed: ', error);
+const fetchBreedDescription = function(breedName, callback) {
+  const breedURL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
   
-  const data = JSON.parse(body);
-  if (data.length === 0) return console.log('Error: Breed not found.');
-  console.log(data[0].breeds[0].description);
-});
+  request(breedURL, (error, response, body) => {
+    if (error) return console.log('Request Failed: ', error);
+    
+    const data = JSON.parse(body);
+    if (data.length === 0) return callback(error, null);
+    // return console.log('Error: Breed not found.');
+    // // console.log(data[0].breeds[0].description);
+
+    callback(null, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
